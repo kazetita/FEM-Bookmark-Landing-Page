0c4d7384a052a7e5547d5b2599ac73623daf657e
Vergara Marcos <vergaramarcoskz@gmail.com>
2022-02-10 00:25:09 -0300

    Add & Fix: Added Mobile version of Features Section and minor fixes to general components and styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// NAV
 const btnToggleNav = document.querySelector('.nav-primary__toggle');
 const menuWrapper = document.querySelector('.nav-primary__menu-wrapper');
 const logoNav = document.querySelector('.nav-primary .logo');
@@ -9,4 +10,40 @@ const toggleMenu = function(e) {
 };
 
 btnToggleNav.addEventListener('click', toggleMenu);
+
+// TAB LIST
+const tabList = document.querySelector('.tab-list');
+const tabListNavItems = tabList.querySelectorAll('.tab-list__nav-item');
+let currentActiveNavItem= tabList.querySelector('.tab-list__nav-item--active');
+
+const isTabActive = tab => tab.classList.contains('tab-list__nav-item--active');
+const getTabItem = tab => tabList.querySelector(tab.children[0].getAttribute('href'));
+
+const desactiveTab = function(tab) {
+  if(!isTabActive(tab)) return;
+
+  const tabItem = getTabItem(tab);
+  tab.classList.remove('tab-list__nav-item--active');
+  tabItem.classList.remove('tab-list__item--active');
+};
+
+const activeTab = function(tab) {
+  if(isTabActive(tab)) return;
+
+  const tabItem = getTabItem(tab);
+  tab.classList.add('tab-list__nav-item--active');
+  tabItem.classList.add('tab-list__item--active');
+}
+
+const handlerNavItemOnClick = function(e) {
+  e.preventDefault();
+  if(isTabActive(this)) return;
+
+  desactiveTab(currentActiveNavItem);
+  activeTab(this);
+  currentActiveNavItem = this;
+};
+
+tabListNavItems.forEach(tab => tab.addEventListener('click', handlerNavItemOnClick));
+
 
