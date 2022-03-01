c634d419d5c7e0ae6d967d61b09a1bb698bbf826
Vergara Marcos <vergaramarcoskz@gmail.com>
2022-02-28 22:39:56 -0300

    Refactor: Primary Nav, README

--- js/main.js
+++ js/main.js
@@ -1,127 +1,118 @@
 // NAV
-// const btnToggleNav = document.querySelector('.nav-primary__toggle');
-// const menuWrapper = document.querySelector('.nav-primary__menu-wrapper');
-// const logoNav = document.querySelector('.nav-primary .logo');
+const mainNav = document.querySelector(".nav-primary");
+const btnToggleNav = mainNav.querySelector(".nav-primary__toggle");
 
-// const toggleMenu = function(e) {
-//   this.classList.toggle('nav-primary__toggle--open');
-//   menuWrapper.classList.toggle('nav-primary__menu-wrapper--visible');
-//   logoNav.classList.toggle('logo--light-on-sm');
-// };
-
-// btnToggleNav.addEventListener('click', toggleMenu);
-
-const mainNav = document.querySelector('.nav-primary');
-const btnToggleNav = mainNav.querySelector('.nav-primary__toggle');
-const menu = mainNav.querySelector('.nav-primary__menu');
-const logoNav = mainNav.querySelector('.logo');
-const isMenuOpen = () => mainNav.classList.contains('nav-primary--open');
-
-const toggleMenu = function(e) {
-  mainNav.classList.toggle('nav-primary--open');
-  logoNav.classList.toggle('logo--light-on-sm');
-  document.body.classList.toggle('no-scroll');
-  this.setAttribute('aria-expanded', String(isMenuOpen()));
+const isMenuOpen = () => mainNav.classList.contains("nav-primary--open");
+const toggleMenu = function (e) {
+  mainNav.classList.toggle("nav-primary--open");
+  this.setAttribute("aria-expanded", String(isMenuOpen()));
 };
 
-btnToggleNav.addEventListener('click', toggleMenu);
+btnToggleNav.addEventListener("click", toggleMenu);
 
 // TAB LIST
-const tabList = document.querySelector('.tab-list');
-const tabListNavItems = tabList.querySelectorAll('.tab-list__nav-item');
-let currentActiveNavItem= tabList.querySelector('.tab-list__nav-item--active');
+const tabList = document.querySelector(".tab-list");
+const tabListNavItems = tabList.querySelectorAll(".tab-list__nav-item");
+let currentActiveNavItem = tabList.querySelector(".tab-list__nav-item--active");
 
-const isTabActive = tab => tab.classList.contains('tab-list__nav-item--active');
-const getTabItem = tab => tabList.querySelector(tab.children[0].getAttribute('href'));
+const isTabActive = (tab) =>
+  tab.classList.contains("tab-list__nav-item--active");
+const getTabItem = (tab) =>
+  tabList.querySelector(tab.children[0].getAttribute("href"));
 
-const desactiveTab = function(tab) {
-  if(!isTabActive(tab)) return;
+const desactiveTab = function (tab) {
+  if (!isTabActive(tab)) return;
 
   const tabItem = getTabItem(tab);
-  tab.classList.remove('tab-list__nav-item--active');
-  tabItem.classList.remove('tab-list__item--active');
+  tab.classList.remove("tab-list__nav-item--active");
+  tabItem.classList.remove("tab-list__item--active");
 };
 
-const activeTab = function(tab) {
-  if(isTabActive(tab)) return;
+const activeTab = function (tab) {
+  if (isTabActive(tab)) return;
 
   const tabItem = getTabItem(tab);
-  tab.classList.add('tab-list__nav-item--active');
-  tabItem.classList.add('tab-list__item--active');
-}
+  tab.classList.add("tab-list__nav-item--active");
+  tabItem.classList.add("tab-list__item--active");
+};
 
-const handlerNavItemOnClick = function(e) {
+const handlerNavItemOnClick = function (e) {
   e.preventDefault();
-  if(isTabActive(this)) return;
+  if (isTabActive(this)) return;
 
   desactiveTab(currentActiveNavItem);
   activeTab(this);
   currentActiveNavItem = this;
 };
 
-tabListNavItems.forEach(tab => tab.addEventListener('click', handlerNavItemOnClick));
+tabListNavItems.forEach((tab) =>
+  tab.addEventListener("click", handlerNavItemOnClick)
+);
 
 //ACCORDION
-const accordion = document.querySelector('.accordion');
-const accordionItems = [... accordion.children];
+const accordion = document.querySelector(".accordion");
+const accordionItems = [...accordion.children];
 
-const handlerAccordionItemOnClick = e => e.currentTarget.classList.toggle('accordion__item--open');
+const handlerAccordionItemOnClick = (e) =>
+  e.currentTarget.classList.toggle("accordion__item--open");
 
-accordionItems.forEach(item => item.addEventListener('click', handlerAccordionItemOnClick));
+accordionItems.forEach((item) =>
+  item.addEventListener("click", handlerAccordionItemOnClick)
+);
 
 //FORM
 
 // Variables
-const formContact = document.querySelector('#form-contact');
+const formContact = document.querySelector("#form-contact");
 let timer;
 
 // General Functions
-const clearErrorOnField = function(form, fieldName) {
+const clearErrorOnField = function (form, fieldName) {
   const field = form[fieldName];
-  if(!field) return null;
-  const formGroup = field.closest('.form__group');
+  if (!field) return null;
+  const formGroup = field.closest(".form__group");
 
-  return function() {
+  return function () {
     delete formGroup.dataset.errorMsg;
-    formGroup.classList.remove('form__group--error');
-  }
+    formGroup.classList.remove("form__group--error");
+  };
 };
 
-const setErrorOnField = function(form, fieldName) {
+const setErrorOnField = function (form, fieldName) {
   const field = form[fieldName];
-  if(!field) return null;
-  const formGroup = field.closest('.form__group');
+  if (!field) return null;
+  const formGroup = field.closest(".form__group");
 
-  return function(msg) {
+  return function (msg) {
     formGroup.dataset.errorMsg = msg;
-    formGroup.classList.add('form__group--error');
-  }
+    formGroup.classList.add("form__group--error");
+  };
 };
 
 // Event handlers
-const handlerFormOnSubmit = function(e) {
+const handlerFormOnSubmit = function (e) {
   e.preventDefault();
-  
-  const email = this['email'];
 
-  if(email.validity.valueMissing) {
-    setErrorOnEmail('Email is required');
+  const email = this["email"];
+
+  if (email.validity.valueMissing) {
+    setErrorOnEmail("Email is required");
     timer = setTimeout(() => clearErrorOnEmail(), 3000);
   }
 
-  if(email.validity.typeMismatch) {
-    setErrorOnEmail('Whoops, make sure it\'s an email');
+  if (email.validity.typeMismatch) {
+    setErrorOnEmail("Whoops, make sure it's an email");
     timer = setTimeout(() => clearErrorOnEmail(), 3000);
   }
-}
+};
 
-const handlerEmailOnFocus = function() {
+const handlerEmailOnFocus = function () {
   clearTimeout(timer);
   clearErrorOnEmail();
 };
 
 //main
-const clearErrorOnEmail = clearErrorOnField(formContact, 'email');
-const setErrorOnEmail = setErrorOnField(formContact, 'email');
-formContact.addEventListener('submit', handlerFormOnSubmit);
-formContact['email'].addEventListener('focus', handlerEmailOnFocus);
+const clearErrorOnEmail = clearErrorOnField(formContact, "email");
+const setErrorOnEmail = setErrorOnField(formContact, "email");
+formContact.addEventListener("submit", handlerFormOnSubmit);
+formContact["email"].addEventListener("focus", handlerEmailOnFocus);
